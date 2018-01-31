2db4fcc4c1086ae9bc24523df5c0858c72dbca21
Venkata Narasimham Peetla <venkata.n.peetla@kp.org>
2018-01-30 17:20:56 -0800

    added function to get product of all indices except that index.

--- ProductOfAllIndexesExcetAtIndex/ViewController.m
+++ ProductOfAllIndexesExcetAtIndex/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSArray *inputArray = @[@1, @3, @5, @7];
+    NSArray *resultArray = GetProductsOfAllIndexesExceptAtIndexFromArray(inputArray);
+    NSLog(@"resultArray is %@", resultArray);
 }
 
 
@@ -25,5 +29,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+NSArray<NSNumber *> *GetProductsOfAllIndexesExceptAtIndexFromArray(NSArray<NSNumber *> *inputArray) {
+    NSCAssert(inputArray.count > 1, @"Invalid parameter: getting the product of numbers at other indices requires at least two elements in array");
+    NSMutableArray<NSNumber *> *outputArray = [NSMutableArray array];
+
+    NSNumber *product = @1;
+
+    //Step:1 getting the product of before index.
+    for (NSUInteger i = 0; i < inputArray.count; i++) {
+        outputArray[i] = product;
+        product = @(product.integerValue * inputArray[i].integerValue);
+    }
+    NSLog(@"outputArray -  getting the product of before index %@", outputArray);
+
+    product = @1;
+    //Step:2 getting the product of after index.
+    for (NSUInteger i = inputArray.count; i > 0; i--) {
+        outputArray[i-1] = @(outputArray[i-1].integerValue * product.integerValue);
+        product = @(product.integerValue * inputArray[i-1].integerValue);
+    }
+    NSLog(@"outputArray - getting the product of after index %@", outputArray);
+
+    //final output array.
+    return outputArray;
+}
 
 @end
